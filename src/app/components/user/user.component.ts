eef357796800b7e76d64beefb42602da861984c1
Shaker Kattoula <smkattoula@gmail.com>
2020-12-18 11:49:06 -0800

    string interpolation and learning typescript

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from "@angular/compiler";
 import { Component } from "@angular/core";
 
 @Component({
@@ -8,23 +9,48 @@ import { Component } from "@angular/core";
 
 export class UserComponent {
     // Properties
-    firstName = "John";
-    lastName = "Doe";
-    age = 30;
+    firstName: string;
+    lastName: string;
+    age: number;
+    address;
+
+    foo: any;
+    hasKids: boolean;
+    numberArray: number[];
+    stringArray: string[];
+    mixedArray: any[];
+    myTuple: [string, number, boolean];
+    unusable: void;
+    undefined: undefined;
+    null: null;
+
 
     // Methods
     constructor() {
-        // this.sayHello()
-        console.log(this.age)
-        this.hasBirthday()
-        console.log(this.age)
+       this.firstName = "John";
+       this.lastName = "Doe";
+       this.age = 30;
+
+       this.address = {
+           street: "123 Sunglow Drive",
+           city: "Oceanside",
+           state: "CA"
+       }
+
+       this.foo = true;
+       this.hasKids = false;
+       this.numberArray = [1,2,3];
+       this.stringArray = ["Hello world"];
+       this.mixedArray = [true, undefined, "hello", 2];
+       this.myTuple = ["hello", 1, true];
+       this.unusable = undefined;
+       this.undefined = undefined;
+       this.null = null;
+
+       console.log(this.addNumbers(2,3));
     }
 
-    sayHello() {
-        console.log(`Hello ${this.firstName}`)
-    }
-
-    hasBirthday() {
-        this.age += 1;
+    addNumbers(num1:number, num2:number): number {
+        return num1 + num2
     }
 }
