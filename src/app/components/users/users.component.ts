45079be893bfc25b94ff88c6ab7acb676240d48d
Shaker Kattoula <smkattoula@gmail.com>
2020-12-19 17:05:37 -0800

    learn loops with ngFor in angular

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/User";
 
 @Component({
   selector: 'app-users',
@@ -6,11 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log("init...")
+    this.users = [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        age: 31,
+        address: {
+            street: "50 Main Street,",
+            city: "San Diego,",
+            state: "CA"
+        }
+      },
+      {
+        firstName: "Tony",
+        lastName: "Ibrahim",
+        age: 24,
+        address: {
+            street: "123 Florentine Drive,",
+            city: "Farmington Hills,",
+            state: "MI"
+        }
+      },
+      {
+        firstName: "Sarah",
+        lastName: "Smith",
+        age: 21,
+        address: {
+            street: "555 Horseshoe Road,",
+            city: "Houston,",
+            state: "TX"
+        }
+      }
+      
+    ];
+
+    this.addUser({
+      firstName: "David",
+      lastName: "Jackson",
+      age: 32,
+      address: {
+          street: "12 Groove St,",
+          city: "Portland,",
+          state: "OR"
+      }
+    })
+  }
+
+  addUser(user: User) {
+    this.users.push(user)
   }
 
 }
